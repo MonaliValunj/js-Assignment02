25aeb91277deb6656ac11881a54c04c9addeb909
Monali Valunj <monavalunj96@gmail.com>
2023-07-02 07:38:05 -0700

    start of the hitchhikers game

---
+++ js/05-hitchhikers-guide.js
@@ -0,0 +1,20 @@
+let response = window.prompt("Are you ready to play?(yes/no)");
+
+if(response === "yes"){
+    window.alert("You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall.") 
+    let direction = window.prompt("Which direction would you like to head (please enter forward, left, or right).")  
+
+    switch(direction) {
+        case "forward":
+          
+          break;
+        case "left":
+          
+          break;
+        case "right":
+          
+          default: 
+      }
+}else{
+    window.alert("Thank you! Play again sometime.")
+}
