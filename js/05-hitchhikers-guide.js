36c2c2bbd0b19e3a5340b0d8eb40da207a80b0c0
Monali Valunj <monavalunj96@gmail.com>
2023-07-02 08:38:37 -0700

    finished the final task

--- js/05-hitchhikers-guide.js
+++ js/05-hitchhikers-guide.js
@@ -6,15 +6,28 @@ if(response === "yes"){
 
     switch(direction) {
         case "forward":
-          
+          window.alert("You walk 100 yards and safely make your way out of the cave.")
           break;
         case "left":
-          
+          window.alert("Your thirst has gotten the better of you. You trip on a rock, hit your head, and fall into a pool of water and drown!")
           break;
         case "right":
-          
+          window.alert("You found the gold! You walk into a small room and see thousands of gold coins, jewels, chalices, and more. You jump into the pile of gold in celebration and immediately a hidden door slams down and traps you in the room forever!")
+          break;
           default: 
-      }
+          window.alert("The ghost of Captain Chingadera has condemned you to eternal damnation and you shall now burn in the hot excoriation for lifeless lowlifes for not choosing the correct option.")
+          console.log(rating)       
+      } // end of switch loop
+      let rating = window.prompt("Great job playing!! /n How would you rate the game (on a scale of 1 to 10)?")
+
+          if(rating >= 6 && rating <=10){
+            window.alert("Thank you!! Play again!")
+          }else if(rating <= 5){
+            window.alert("Thank you!! We are working hard to improve the game.")
+          }else{
+            window.alert("Thanks for playing.s")
+          } // of checking raing
+      //window.alert("Game is over.") 
 }else{
     window.alert("Thank you! Play again sometime.")
 }
